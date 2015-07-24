8e996f54a221ad8ae7fd66889831849b6d1386ac
Yoshito Imai <yoshito.imai@gmail.com>
2015-07-24 14:54:23 +0900

    change button

--- custom.js
+++ custom.js
@@ -10,12 +10,14 @@ gameStart = function(core) {
 	var button_left = new Sprite(100, 24);
 	button_left.image = core.assets['button_left.png'];
 	button_left.x = 0;
-	button_left.y = 0;
+	button_left.y = 456;
 	core.rootScene.addChild(button_left);
-
 
 	// 右へ移動するボタン
 	var button_right = new Sprite(100, 24);
-
+	button_right.image = core.assets['button_right.png'];
+	button_right.x = 221;
+	button_right.y = 456;
+	core.rootScene.addChild(button_right);
 
 };
